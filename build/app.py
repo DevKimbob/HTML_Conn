48bd4b90f123142659af00c0e6ea0f965831d87a
devkimbob <devkimbob@gmail.com>
2023-06-13 04:59:34 -0700

    .

--- build/app.py
+++ build/app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 print('Container name : ', socket.gethostname())
 print('Container IP : ', socket.gethostbyname(socket.gethostname()), end='\n\n')
 
+file = open('version.txt', 'r')
+version = file.read().split('=')[1]
+file.close()
+
 @app.route("/")
 def hello_world():
-    html = f"Version : 1.5.0\nContainer name : {socket.gethostname()}\nContainer IP : {socket.gethostbyname(socket.gethostname())}\n"
+    html = f"Version : {version}\nContainer name : {socket.gethostname()}\nContainer IP : {socket.gethostbyname(socket.gethostname())}\n"
     # html = f"<body style='background-color:{os.environ.get('BG_COLOR')};'>\
     # <h1 style='color:{os.environ.get('FONT_COLOR')}'>{os.environ.get('CUSTOM_HEADER')}</h1> \
     # <h2 style='color:{os.environ.get('FONT_COLOR')};'>Hello World! Served from <b>{socket.gethostname()}</b></h2></body>"
